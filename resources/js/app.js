12fa820341f84f0dce472b0eecf51aa81a16b29f
yuta10200831 <yfuse20ki@yahoo.co.jp>
2024-05-20 19:30:59 +0900

    修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+window.Alpine = Alpine;
+
+Alpine.plugin(focus);
+
+Alpine.start();
