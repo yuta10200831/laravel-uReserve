29e5fbd51b2adf42b92aeb93122c29d5a4c4aa6b
yuta10200831 <yfuse20ki@yahoo.co.jp>
2024-05-20 23:57:37 +0900

    jetStreamの修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,4 @@
 import './bootstrap';
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import './bootstrap';
 
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
