c768582ebf7003d43ed3ccac380e1aa5b28864e6
yuta10200831 <yfuse20ki@yahoo.co.jp>
2024-06-04 23:21:41 +0900

    event store

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,6 +6,9 @@ use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
 use App\Models\Event;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Services\EventService;
+
 
 class EventController extends Controller
 {
@@ -42,7 +45,30 @@ class EventController extends Controller
      */
     public function store(StoreEventRequest $request)
     {
-        //
+        $check = EventService::checkEventDuplication(
+            $request['event_date'],$request['start_time'],$request['end_time']);
+
+        if($check){
+            session()->flash('status', '既に埋まっています');
+            return view('manager.events.create');
+        }
+
+        $startDate = EventService::joinDateAndTime($request['event_date'],$request['start_time']);
+        $endDate = EventService::joinDateAndTime($request['event_date'],$request['end_time']);
+
+        Event::create([
+            'name' => $request['event_name'],
+            'information' => $request['information'],
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'max_people' => $request['max_people'],
+            'is_visible' => $request['is_visible']
+        ]);
+
+        session()->flash('status', '登録OK');
+
+        return to_route('events.index');
+
     }
 
     /**
--- app/Http/Requests/StoreEventRequest.php
+++ app/Http/Requests/StoreEventRequest.php
@@ -13,7 +13,7 @@ class StoreEventRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +23,14 @@ class StoreEventRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
-        ];
+        { return [
+            'event_name' => ['required', 'max:50'],
+            'information' => ['required', 'max:200'],
+            'event_date' => ['required', 'date'],
+            'start_time' => ['required'],
+            'end_time' => ['required', 'after:start_time'],
+            'max_people' => ['required', 'numeric', 'between:1,20'],
+            'is_visible' => ['required', 'boolean']
+            ];}
     }
 }
