83cefd923a3e92f63cbaa1112e6b59b4505732eb
yuta10200831 <yfuse20ki@yahoo.co.jp>
2024-06-08 16:31:23 +0900

    event dummy 修正

---
+++ resources/views/calendar.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="csrf-token" content="{{ csrf_token() }}">
+
+ <title>{{ config('app.name', 'Laravel') }}</title>
+
+ <!-- Fonts -->
+ <link rel="preconnect" href="https://fonts.bunny.net">
+ <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+ <!-- Scripts -->
+ @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/flatpickr.js'])
+
+ <!-- Styles -->
+ @livewireStyles
+</head>
+
+<body class="font-sans antialiased">
+ @livewire('calendar')
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+ @vite('resources/js/flatpickr.js')
+ @livewireScripts
+</body>
+
+</html>
---
+++ resources/views/livewire/calendar.blade.php
@@ -0,0 +1,11 @@
+<div>
+ カレンダー
+ <input id="calendar" class="block mt-1 w-full" type="text" name="calender" value="{{ $currentDate }}"
+  wire:change="getDate($event.target.value)" />
+ {{ $currentDate }}
+ <div class="flex">
+  @for($day = 0; $day < 7; $day++) {{ $currentWeek[$day] }} @endfor </div>
+   @foreach( $events as $event )
+   {{ $event->start_date }}
+   @endforeach
+ </div>
