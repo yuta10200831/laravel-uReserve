8a52537e797882e39aa441c904992479f7dcc003
yuta10200831 <yfuse20ki@yahoo.co.jp>
2024-06-12 16:01:26 +0900

    event reserve

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivewireTestConttoller;
 use App\Http\Controllers\AlpineTestController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,15 +21,15 @@ Route::get('/', function () {
     return view('calendar');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
 
 Route::prefix('manager')
 ->middleware('can:manager-higher')
@@ -39,15 +40,16 @@ Route::prefix('manager')
 
 Route::middleware('can:user-higher')
 ->group(function(){
-    Route::get('index', function () {
-        dd('user');
-        });
+    Route::get('/dashboard', [ReservationController::class, 'dashboard'])->name('dashboard');
+    Route::get('/{id}', [ ReservationController::class, 'detail' ] )->name('events.detail');
+    Route::post('/{id}', [ ReservationController::class, 'reserve' ] )->name('events.reserve');
+
 });
 
 Route::controller(LivewireTestConttoller::class)
 ->prefix('livewire-test')->name('livewire-test.')->group(function(){
- Route::get('index', 'index')->name('index');
- Route::get('register', 'register')->name('register');
+    Route::get('index', 'index')->name('index');
+    Route::get('register', 'register')->name('register');
 });
 
 Route::get('alpine-test/index', [AlpineTestController::class, 'index']);
